1cfe7ef41742b658f2ea1a1ee4212305a24ea422
juanjoseferrari <63857163+juanjoseferrari@users.noreply.github.com>
2020-04-23 22:07:52 -0300

    Add files via upload

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,12 @@
+/* FERRARI, JUAN JOSE
+* 20200423
+* SUMAR DOS NUMEROS
+*/
+#include <iostream>
+int main(){
+    int x,y;
+    std::cin >> x;
+    std::cin >> y;
+    std::cout << "Suma: " << x+y;
+    system ("pause");
+}
